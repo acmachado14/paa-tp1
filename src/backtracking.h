b63145c27508cd5868ee901bfe2ed0a5cb09bb65
Iurymartins46 <martinsiury46@gmail.com>
2022-11-02 13:23:18 -0300

    :sparkles: feat: Inicialização, leitura e insersão dos dados

---
+++ src/backtracking.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+typedef struct{
+    int **campo;
+    int N;
+    int M;
+}Fazenda;
+
+bool leituraDados(Fazenda *fazenda);
+void inicializaMatriz(Fazenda *fazenda, int N, int M);
+void inserirNumeroCampo(Fazenda *fazenda, int  numeroCampo,int i, int j);
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
-void main(){
+#include "backtracking.h"
 
+void main(){
     
 }
